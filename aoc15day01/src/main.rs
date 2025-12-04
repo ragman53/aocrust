75707b564b4ef49eeaa1ff18586a8b7ff312f0b5
Junya Kobayashi <ragman372@gmail.com>
2025-12-04 22:20:24 +0900

    solved 2015 day1 part 1

--- aoc15day01/src/main.rs
+++ aoc15day01/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let path = Path::new("aoc15day01/input.txt");
+    let input = match fs::read_to_string(path) {
+        Ok(input) => input,
+        Err(e) => return Err(Box::new(e)),
+    };
+
+    let mut counter: i32 = 0;
+    for c in input.chars() {
+        match c {
+            '(' => counter += 1,
+            ')' => counter -= 1,
+            _ => (),
+        }
+    }
+    println!("floor is {:?}", counter);
+    Ok(())
 }
