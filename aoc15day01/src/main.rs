b866fba8b416c5a292327bd77095653041fd7e02
Junya Kobayashi <ragman372@gmail.com>
2025-12-05 00:00:24 +0900

    solved 2015 day1 part 2 EDIT intergrate part1 & part2

--- aoc15day01/src/main.rs
+++ aoc15day01/src/main.rs
@@ -3,17 +3,19 @@ use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let path = Path::new("aoc15day01/input.txt");
-    let input = match fs::read_to_string(path) {
-        Ok(input) => input,
-        Err(e) => return Err(Box::new(e)),
-    };
+    let input = fs::read_to_string(path)?;
+    let mut basement: Option<usize> = None;
 
     let mut counter: i32 = 0;
-    for c in input.chars() {
+    for (i, c) in input.char_indices() {
         match c {
             '(' => counter += 1,
             ')' => counter -= 1,
             _ => (),
+        }
+        if counter == -1 && basement.is_none() {
+            println!("floor -1: {}", i + 1);
+            basement = Some(i + 1);
         }
     }
     println!("floor is {:?}", counter);
