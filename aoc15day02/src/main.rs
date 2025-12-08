ac9ce1f372ebe5b81a4a07f0c0254f744a92f817
Junya Kobayashi <ragman372@gmail.com>
2025-12-08 23:08:57 +0900

    solved 2015 day 01 part1

---
+++ aoc15day02/src/main.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use std::fs;
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Gift {
+    l: u32,
+    w: u32,
+    h: u32,
+}
+
+impl FromStr for Gift {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut dims: Vec<u32> = s
+            .split("x")
+            .map(|x| x.parse::<u32>())
+            .collect::<Result<Vec<_>, _>>()?;
+        dims.sort();
+        if dims.len() != 3 {
+            return Err("Invalid dimensions count".into());
+        }
+        Ok(Gift {
+            l: dims[0],
+            w: dims[1],
+            h: dims[2],
+        })
+    }
+}
+
+impl Gift {
+    fn paper_sum(&self) -> u32 {
+        let slack: u32 = self.l * self.w;
+        let surface: u32 = 2 * (self.l * self.w + self.w * self.h + self.h * self.l);
+        slack + surface
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut paper_needed: u32 = 0;
+    let input = fs::read_to_string("input.txt")?;
+    for (i, line) in input.lines().enumerate() {
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        let gift: Gift = line.parse::<Gift>()?;
+        println!("Line {} {:?}", i, gift);
+        paper_needed += gift.paper_sum();
+    }
+    println!("Total paper: {}", paper_needed);
+    Ok(())
+}
