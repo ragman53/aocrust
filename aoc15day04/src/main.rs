ceb32dc1c965c785a56941cb66f46a9c8d384535
Junya Kobayashi <ragman372@gmail.com>
2025-12-14 16:01:23 +0900

    Solved 2015 day 04 part2

--- aoc15day04/src/main.rs
+++ aoc15day04/src/main.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     for i in 1..=u64::MAX {
         let input = format!("{}{}", secret_key, i);
         let digest = md5::compute(input);
-        if digest[0] == 0 && digest[1] == 0 && digest[2] < 16 {
+        if digest[0] == 0 && digest[1] == 0 && digest[2] == 0 {
             println!("Answer: {}", i);
             println!("Hash: {:x}", digest);
             break;
