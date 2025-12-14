03ad3f7d32b2fc260bc07ec39edd12e71e965747
Junya Kobayashi <ragman372@gmail.com>
2025-12-14 15:57:15 +0900

    Solved 2015 Day04 part 1

--- aoc15day04/src/main.rs
+++ aoc15day04/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let secret_key = "iwrupvqb";
+
+    for i in 1..=u64::MAX {
+        let input = format!("{}{}", secret_key, i);
+        let digest = md5::compute(input);
+        if digest[0] == 0 && digest[1] == 0 && digest[2] < 16 {
+            println!("Answer: {}", i);
+            println!("Hash: {:x}", digest);
+            break;
+        }
+    }
+
+    Ok(())
 }
